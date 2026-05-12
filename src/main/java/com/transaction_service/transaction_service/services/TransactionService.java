78d0e639c25713c5b19401087f10fd80b559177d
ElMaVio <adrian.diaz.rangel@alumnos.cetchile.cl>
2026-05-12 09:16:00 -0400

    conectado a la bd

--- src/main/java/com/transaction_service/transaction_service/services/TransactionService.java
+++ src/main/java/com/transaction_service/transaction_service/services/TransactionService.java
@@ -3,9 +3,11 @@ package com.transaction_service.transaction_service.services;
 import com.transaction_service.transaction_service.models.entities.Transaction;
 import com.transaction_service.transaction_service.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TransactionService {
     @Autowired
     private TransactionRepository transactionrepository;
